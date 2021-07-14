3dde51cb0331cff654edef5cd9788038b3c540db
nilishaUG <82565475+nilishaUG@users.noreply.github.com>
2021-07-14 17:23:55 +0530

    Update Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://18.212.74.66:8080/api/v1/";
+    this.baseUrl = "http://44.192.122.145:8080/api/v1/";
   }
   render(){
     return(
