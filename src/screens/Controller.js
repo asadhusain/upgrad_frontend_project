310a6fd0cc7847fc825930ea8db1c035fc21b322
asadhusain <69382863+asadhusain@users.noreply.github.com>
2021-10-02 16:59:54 +0530

    Update Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://3.83.172.124:8080/api/v1/";
+    this.baseUrl = "http://54.208.108.175:8080/api/v1/";
   }
   render(){
     return(
